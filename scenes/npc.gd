b09bc7b2e7510e3087ae354a51ac7833bd727b8a
susenka44 <vondrackovaj@gvp.cz>
2024-11-10 15:44:52 +0100

    NPC functions

--- scenes/npc.gd
+++ scenes/npc.gd
@@ -5,8 +5,24 @@ var active: bool = false
 
 signal activation
 
+func _ready() -> void:
+	$CanvasLayer/TextureRect.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if active and Input.is_action_just_pressed("Action"):
 		activation.emit()
+
+
+func _on_close_area_area_entered(area: Area2D) -> void:
+	if area is Player:
+		$CanvasLayer/TextureRect.show()
+		active = true
+		
+		for text in converstation:
+			$CanvasLayer/TextureRect/Label.text = text
+			await activation
+
+
+func _on_close_area_area_exited(area: Area2D) -> void:
+	$CanvasLayer/TextureRect.hide()
